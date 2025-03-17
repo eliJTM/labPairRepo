0b827b1e9d3f52edf70fbeab1181b3fbd1d596d1
eliJTM <elijahmenor28@gmail.com>
2025-03-17 12:52:05 +0000

    Passes tests up to testRegisterAndUnregisterMoreThanOneObserver

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
@@ -4,7 +4,12 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * cw-model
@@ -13,9 +18,47 @@ import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 public final class MyModelFactory implements Factory<Model> {
 
 	@Nonnull @Override public Model build(GameSetup setup,
-	                                      Player mrX,
-	                                      ImmutableList<Player> detectives) {
+										  Player mrX,
+										  ImmutableList<Player> detectives) {
 		// TODO
-		throw new RuntimeException("Implement me!");
+		return new MyModel(null);}
+	private final class MyModel implements Model {
+		private GameState state;
+		private Set<Observer> observers;
+
+		private MyModel (GameState state) {
+			this.state = state;
+			this.observers = new HashSet<>();
+			}
+
+
+		@Nonnull
+		@Override
+		public Board getCurrentBoard() {
+			return state;
+		}
+
+		@Override
+		public void registerObserver(Observer observer) {
+			if(observer == null) throw new NullPointerException("Observer can not be null!");
+			observers.add(observer);
+		}
+
+		@Override
+		public void unregisterObserver(Observer observer) {
+			if(observer == null) throw new NullPointerException("Observer can not be null!");
+			observers.remove(observer);
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Observer> getObservers() {
+			return ImmutableSet.copyOf(observers);
+		}
+
+		@Override
+		public void chooseMove(Move move) {
+
+		}
 	}
 }
